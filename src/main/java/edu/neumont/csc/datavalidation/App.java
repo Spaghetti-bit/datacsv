8ec99ebaefe04d6a31c9488159bb8947cac5aa22
Sage <Cvaughn@student.neumont.edu>
2022-10-26 12:23:16 -0600

    DataValidation RegexUtil Person App Main changes

--- src/main/java/edu/neumont/csc/datavalidation/App.java
+++ src/main/java/edu/neumont/csc/datavalidation/App.java
@@ -1,17 +1,54 @@
 package edu.neumont.csc.datavalidation;
 
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
 public final class App {
     private App() {
     }
 
     /**
-     * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\LaptopNeumont-Sage\\CSC180 Workspace DIR\\dataextraction\\people-2.to.regex.csv"));
+            String line = "";
+            Person person = new Person();
+            DataValidation validator = new DataValidation();
+            Field[] personFields = Person.class.getFields();
+            Method[] dataMethods = DataValidation.class.getMethods();
+            while ((line = br.readLine()) != null)
+            {
+                // CSV :)
+                String[] personStrArray = line.split(",");
+                // Iterate through fields AND the personStrArray, hydrate with valid data.
+                for (int i = 0; i < personStrArray.length && i < personFields.length; i++)
+                {
+                    //Validate data.
+                    /*
+                    * name
+                    * ssn
+                    * email
+                    * phone
+                    */
+                    Object result = dataMethods[i].invoke(validator, personStrArray[i]);
+                    if (result instanceof Boolean)
+                    {
+                        Boolean valid = (Boolean) result;
+                        //If Data is valid, set it.
+                        if(valid) personFields[i].set(person, personStrArray[i]);
+                    }
+                }
+            }
+        } catch (Exception e)
+        {
+
+        }
     }
 }
---
+++ src/main/java/edu/neumont/csc/datavalidation/Person.java
@@ -0,0 +1,8 @@
+package edu.neumont.csc.datavalidation;
+
+public class Person {
+    public String name;
+    public String ssn;
+    public String email;
+    public String phoneNumber;
+}
